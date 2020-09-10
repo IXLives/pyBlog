babc39576bca9d7a161b83d534cda6e632424515
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2020-09-10 19:22:48 -0400

    unspace

--- pyBlog/blog.py
+++ pyBlog/blog.py
@@ -11,11 +11,10 @@ bp = Blueprint('blog', __name__)
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN users u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
+    posts = db.execute('SELECT p.id, title, body, created, author_id, username'
+                       ' FROM post p JOIN users u ON p.author_id = u.id'
+                       ' ORDER BY created DESC'
+                       ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
 
